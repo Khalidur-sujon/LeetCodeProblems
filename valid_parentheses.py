9805d3d085225e6e98d2283c992d326fb0cf14a6
Khalidur Rahman <99888997+Khalidur-sujon@users.noreply.github.com>
2022-10-02 14:19:02 +0600

    Add files via upload

---
+++ valid_parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        open_to_close = {')':'(', '}':'{', ']':'['}
+
+        for c in s:
+            if c in open_to_close:
+                if stack and stack[-1] == open_to_close[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
