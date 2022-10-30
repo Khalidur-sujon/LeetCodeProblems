9d8383c91880b4673d52f25be5d1a2e642f10b72
Khalidur Rahman <99888997+Khalidur-sujon@users.noreply.github.com>
2022-10-30 11:21:42 +0600

    Add files via upload

---
+++ SwapNodesInPairs.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev, curr = dummy, head
+        while curr and curr.next:
+            # save pointers 
+            nxt = curr.next.next
+            second = curr.next
+
+            # reverse this pair
+            second.next = curr
+            curr.next = nxt
+            prev.next = second
+
+            # update the pointer 
+            prev = curr
+            curr = nxt 
+
+        return dummy.next
